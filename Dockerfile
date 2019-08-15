7a0196a5c950d8fa07ed7471f141e55edd334a6b
Graves, Mark <mark.graves@tylertech.com>
2019-08-15 15:15:44 -0500

    Removed unused features from the boostrap.sh script

--- Dockerfile
+++ Dockerfile
@@ -1,7 +1,5 @@
 FROM appropriate/curl:latest
 COPY ./config/bootstrap.sh /init/
 ENV APICONTAINER check-yo-self-api
-# ENV CONSULCONTAINER consul:8500
-# ENV MANIFESTCONTAINER intents
 RUN ["dos2unix", "/init/bootstrap.sh"]
 ENTRYPOINT [ "sh", "-c", "/init/bootstrap.sh" ]
--- config/bootstrap.sh
+++ config/bootstrap.sh
@@ -1,23 +1,5 @@
 #!/bin/sh
 
-# If the container name has been set, then bootstrap that container
-if [ ! -z "$CONSULCONTAINER" ]
-then
-    # To ensure we've given an istio sidecar time to bootstrap, we'll wait 2 seconds
-    echo 'testing connection to consul'
-    sh -c 'while [[ "$(curl -s -o /dev/null -w ''%{http_code}'' http://${CONSULCONTAINER}/v1/kv/?keys)" != "200" ]]; do sleep 1; done'
-    echo 'connection to consul successful'
-
-    echo 'bootstrapping consul'
-    curl --request PUT --data @/config/appsettings.json http://${CONSULCONTAINER}/v1/kv/environment.json -H "Content-Type:application/json"
-fi
-
-if [ ! -z "$MANIFESTCONTAINER" ]
-then
-    echo 'bootstrapping manifestservice'
-    curl --request POST --data @/config/manifests.json http://${MANIFESTCONTAINER}/WebApplications -H "Content-Type:application/json"
-fi
-
 if [ ! -z "$APICONTAINER" ]
 then
     # To ensure we've given an istio sidecar time to bootstrap, we'll wait 2 seconds
