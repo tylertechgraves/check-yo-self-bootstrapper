652ddb61331629fd1d8ce27bd9707e922b3f2bef
Mark Graves <mark.graves@tylertech.com>
2023-02-27 15:55:47 -0600

    fix: Fixed paths after adding versioning to api

--- config/bootstrap.sh
+++ config/bootstrap.sh
@@ -4,9 +4,9 @@ if [ ! -z "$APICONTAINER" ]
 then
     # To ensure we've given an istio sidecar time to bootstrap, we'll wait 2 seconds
     echo 'testing connection to api container'
-    sh -c 'while [[ "$(curl -s -o /dev/null -w ''%{http_code}'' http://${APICONTAINER}:8080/health)" != "200" ]]; do sleep 1; done'
+    sh -c 'while [[ "$(curl -s -o /dev/null -w ''%{http_code}'' http://${APICONTAINER}/health)" != "200" ]]; do sleep 1; done'
     echo 'connection to api successful'
 
     echo 'bootstrapping elasticsearch'
-    curl -X POST "http://${APICONTAINER}:8080/api/Employees/ReindexAllEmployees" -d ""
+    curl -X POST "http://${APICONTAINER}/api/v1/Employees/ReindexAllEmployees" -d ""
 fi
